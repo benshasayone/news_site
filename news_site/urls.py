aed160fd47c3902235d6a8889e759752eddc3b27
atleyvarghese <atleyvarghese@gmail.com>
2017-09-11 10:34:45 +0530

    ajax error

--- news_site/urls.py
+++ news_site/urls.py
@@ -20,7 +20,7 @@ from news_site import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
-    url(r'', include('news.urls', namespace='news')),
+    url(r'^', include('news.urls', namespace='news')),
     url(r'^accounts/', include('allauth.urls')),
     url(r'^admin/', admin.site.urls),
     url(r'^account/',include('accounts.urls')),
